c0860817ed1255953932010a2c64c384b9296a2e
Sebastian <novvac21@protonmail.com>
2021-05-24 16:20:32 +0200

    created mui theme and changed default font

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme/index';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/theme/index.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const font = "'Titillium Web', sans-serif"
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: font,
+    }
+})
+
+export default theme;
